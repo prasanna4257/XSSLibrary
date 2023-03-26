86703ab90da4bbae14649eb49919df0a3fcb41dd
Prasanna Kumar Deavarapalli <128957604+prasanna4257@users.noreply.github.com>
2023-03-26 13:10:37 +0530

    Create XSSTest.java

---
+++ com/prasanna/xss_fix/XSSTest.java
@@ -0,0 +1,16 @@
+package com.prasanna.xss_fix;
+
+import static com.prasanna.xss_fix.SynataxConstantsInterface.*;
+
+public class XSSTest {
+
+    /* Note: Adding Regex will not reduce the XSS issues count.
+     *  so below approach will not reduce the XSS issues count. */
+
+    public static void main(String... str){
+        XSSUtility.validateString("Prasanna", stringSyntax);
+        XSSUtility.validateString("Prasanna4257", alphaNumeric);
+        XSSUtility.validateString("Prasanna4257@gmail.com", eMailSyntax);
+        XSSUtility.validateString("Prasanna_Kumar", docNameSyntax);
+    }
+}
